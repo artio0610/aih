f0b9df8d9644614a774a5361ac2c031ff70822b6
artio0610 <artio0610@gmail.com>
2014-10-13 14:03:02 +0100

    initial
    initial

---
+++ atuwf/Debug/WString.cpp.d
@@ -0,0 +1,4 @@
+ \
+ C:\Users\osinskia\AppData\Local\VMicro\Arduino\Builds\atuwf\uno\WString.cpp.o:  \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\WString.cpp \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\/WString.h
---
+++ atuwf/Debug/new.cpp.d
@@ -0,0 +1,4 @@
+ \
+ C:\Users\osinskia\AppData\Local\VMicro\Arduino\Builds\atuwf\uno\new.cpp.o:  \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\new.cpp \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/new.h
